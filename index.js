7bf478422ce44113ec7b005e2e3d72ac4dc409da
Derrick Pelletier <derrick@dpelletier.com>
2013-12-04 15:00:06 -0800

    cleaned it up a bit and added some comments in

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ module.exports = function (app, routes, triggerMap, verbose) {
     triggerMap = {};
   }
 
+  var regex = new RegExp("[" + Object.keys(triggerMap).join("") + "]", "g");
+
   /**
    * Recursive mapper function set on app to provide scope
    * Works through nested object of routes, checking for triggers and compiling middleware for routes.
@@ -13,26 +15,44 @@ module.exports = function (app, routes, triggerMap, verbose) {
    * @param <String> route
    * @param <String> force_trigger
    */
-  var mapper = function(a, route, force_trigger){
-    force_trigger = typeof force_trigger === 'undefined' ? false : force_trigger;
+  var mapper = function(ob, route, force_trigger){
+
     route = route || '';
-    for (var key in a) {
+    
+    // Iterate over the current object level
+    for (var key in ob) {
+
+      // Current triggers for this level are either forced parent triggers or empty
       var triggers = force_trigger ? force_trigger.slice() : [];
-      var trigger_check = key.match(new RegExp("[" + Object.keys(triggerMap).join("") + "]", "g"));
+
+      // Check for new triggers on this level and add to trigger stack
+      var trigger_check = key.match(regex);
       if(trigger_check || (!triggers.length && trigger_check)) {
         triggers = triggers.concat(trigger_check);
       }
-      route = route.replace(new RegExp("\[" + Object.keys(triggerMap).join("") + "]", "g"), '');
-      if(!Array.isArray(a[key]) && typeof a[key] !== 'function') {
-        mapper(a[key], route + key, triggers.length ? triggers : false);
+
+      // remove the triggers from the route
+      route = route.replace(regex, '');
+
+      // If this value is just another object, recurse.
+      if(!Array.isArray(ob[key]) && typeof ob[key] !== 'function') {
+        mapper(ob[key], route + key, triggers.length ? triggers : false);
         continue;
       }
-      if(!Array.isArray(a[key])) a[key] = [a[key]];
+
+      // Generate the middleware stack from the triggers
       var mappedMiddleware = triggers.map(function(el){
         return triggerMap[el];
       });
-      var endpoints = [route].concat(mappedMiddleware).concat(a[key]);
+
+      // concatenate the route with the middleware stack and the final endpoint.
+      if(!Array.isArray(ob[key])) ob[key] = [ob[key]];
+      var endpoints = [route].concat(mappedMiddleware).concat(ob[key]);
+
+
       if(verbose) console.log('%s\t\x1B[31m%s\x1B[39m\t%s \x1B[90m(%d)\x1B[39m', key, (triggers.length ? triggers.join("") : '-'), route, endpoints.length - 1);
+
+      // set the callbacks on the currente verb!
       app[key].apply(app, endpoints);
     }
   }
