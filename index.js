52363d4d4079bf7c18819ec3a98d53d71e8e5fbc
Derrick Pelletier <derrick@dpelletier.com>
2013-12-10 12:36:15 -0800

    overhauled mapper, added a bunch of settings behaviour, changed signature of module

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-module.exports = function (app, routes, triggerMap, verbose) {
+module.exports = function (routes, app, opts) {
+  var settings = {
+    triggerMap: {},
+    verbose: false,
+    app: null,
+    processor: function(method, route, endpoints) {
+      if(!settings.app) return console.log("You must specify an express app instance for default functionality.")
+      settings.app[method].apply(settings.app, [route].concat(endpoints));
+    }
+  };
+
+  if(typeof app === 'function') {
+    settings.app = app;
+  } else if(typeof app === 'object') {
+    opts = app;
+  }
+  opts = opts ? opts : {};
   
-  if(typeof triggerMap !== 'object') {
-    verbose = triggerMap;
-    triggerMap = {};
+  for(var i in settings) {
+    settings[i] = opts.hasOwnProperty(i) ? opts[i] : settings[i];
   }
 
-  var regex = new RegExp("[" + Object.keys(triggerMap).join("") + "]", "g");
+  var regex = new RegExp("[" + Object.keys(settings.triggerMap).join("") + "]", "g");
 
   /**
    * Recursive mapper function set on app to provide scope
@@ -13,49 +28,32 @@ module.exports = function (app, routes, triggerMap, verbose) {
    *
    * @param <Object> a (hash map of routes)
    * @param <String> route
-   * @param <String> force_trigger
    */
-  var mapper = function(ob, route, force_trigger){
-
+  var mapper = function(ob, route){
     route = route || '';
-    
-    // Iterate over the current object level
-    for (var key in ob) {
 
-      // Current triggers for this level are either forced parent triggers or empty
-      var triggers = force_trigger ? force_trigger.slice() : [];
-
-      // Check for new triggers on this level and add to trigger stack
-      var trigger_check = key.match(regex);
-      if(trigger_check || (!triggers.length && trigger_check)) {
-        triggers = triggers.concat(trigger_check);
-      }
-
-      // remove the triggers from the route
-      route = route.replace(regex, '');
-
+    Object.keys(ob).forEach(function(key){
       // If this value is just another object, recurse.
       if(!Array.isArray(ob[key]) && typeof ob[key] !== 'function') {
-        mapper(ob[key], route + key, triggers);
-        continue;
+        return mapper(ob[key], route + key);
       }
 
-      // Generate the middleware stack from the triggers
+      // Check for new triggers on this level and add to trigger stack
+      var triggers = route.match(regex) || [];
+
+      // Generate the middleware stack from the triggers and endpoints
       var mappedMiddleware = triggers.map(function(el){
-        return triggerMap[el];
+        return settings.triggerMap[el];
       });
-
-      // concatenate the route with the middleware stack and the final endpoint.
       if(!Array.isArray(ob[key])) ob[key] = [ob[key]];
-      var endpoints = [route].concat(mappedMiddleware).concat(ob[key]);
+      var endpoints = mappedMiddleware.concat(ob[key]);
 
+      if(settings.verbose) console.log('%s\t\x1B[31m%s\x1B[39m\t%s \x1B[90m(%d)\x1B[39m', key, (triggers.length ? triggers.join("") : '-'), route, endpoints.length);
 
-      if(verbose) console.log('%s\t\x1B[31m%s\x1B[39m\t%s \x1B[90m(%d)\x1B[39m', key, (triggers.length ? triggers.join("") : '-'), route, endpoints.length - 1);
-
-      // set the callbacks on the currente verb!
-      app[key].apply(app, endpoints);
-    }
+      // run the processor to create this route
+      settings.processor(key, route.replace(regex, ''), endpoints);
+    });
   }
 
-  mapper(routes, null, null, verbose);
+  mapper(routes, null);
 }
